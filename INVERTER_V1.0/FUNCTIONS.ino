1bc78e1343870dc129527b9be0b1a9f3350de8ea
Mannyquinn <150354697+Mannyquinn@users.noreply.github.com>
2025-08-04 23:40:49 +0100

    Delete INVERTER_V1.0 directory

--- INVERTER_V1.0/FUNCTIONS.ino
+++
@@ -1,9 +0,0 @@
-void softStart()
-{
-  for(int dutyCycleMultiplier = 0; dutyCycleMultiplier < multiplierSoftStartValue; dutyCycleMultiplier++)
-  {
-    multiplier = dutyCycleMultiplier;
-    delay(1);
-  }
-  multiplier = 500;
-}
