7cd0d87f69e307f81bd6eecea3ea34f24fbbe49f
Mannyquinn <150354697+Mannyquinn@users.noreply.github.com>
2025-12-20 12:42:13 +0100

    Add files via upload

---
+++ INVERTER_V1.0/FUNCTIONS.ino
@@ -0,0 +1,171 @@
+/*startup checks, only checks if the input rails are in the right range*/
+void startupCheck()                                                                                                            
+{
+  inputVoltage = (float)analogRead(INPUT_VOLTAGE_FB_PIN) * INPUT_RESISTOR_DIVIDER * voltageScalingFactor;
+  
+ /*make sure that if the voltage is not in the right range, we can't start the pwm or anything, shutdown everything*/
+  if(inputVoltage <= inputVoltageLowValue || inputVoltage >= inputVoltageHighValue)                                                                                  
+  {
+    /* diagnostic variable*/
+    startupError = true;
+    
+    if(inputVoltage >= inputVoltageHighValue){
+      highInputVoltage = true;
+      lowInputVoltage = false;}
+      
+    else if(inputVoltage <= inputVoltageLowValue){
+      lowInputVoltage = true; 
+      highInputVoltage = false;}                                                                                                  
+  }
+  
+  systemErrorCheck();
+}
+
+void softStart()                                                                                                             
+{
+  /*soft start is used when the system first begin, 1. to avoid inrush current, 2. to check for overloading or shorts at startup*/
+  
+  /*if the duty cycle is high and the output voltage isn't up to half the voltage it should be, there is an error*/
+  
+  while(outputVoltage < SET_OUTPUT_VOLTAGE)
+  {
+    /*directly increase the multiplier slowly to perform a softstart*/
+    /*this while loop only runs as long as the output voltage hasn't reached the set value*/ 
+
+    /*if the multiplier is not maximum and the voltage hasn't reached the set voltage*/
+    if(multiplier < MULTIPLIER_MAX_VALUE)                
+      {
+        multiplier++;  
+        delay(2);
+      }
+    /*if the multiplier has started increasing but the voltage hasn't picked up, there might  be a short */
+      else if(multiplier > 250 && outputVoltage < 0.1*SET_OUTPUT_VOLTAGE)
+      {
+        startupError = true;
+        shortCircuit = true;
+        break;
+      }
+
+    /*if the multiplier is maximum and the output voltage hasn't reached the set voltage, most likely overloading or output short */
+    else
+     {
+       multiplier = 0;
+       startupError = true;
+       overload = true;
+       break;
+     }
+
+    /*read the output voltage after a change in multiplier*/ 
+     outputVoltage = (float)analogRead(OUTPUT_VOLTAGE_FB_PIN) * OUTPUT_RESISTOR_DIVIDER * (float)voltageScalingFactor;
+  }
+
+  /*check if there were any errors in the startup*/
+  systemErrorCheck();
+  
+}
+
+/* this system check if there is any issue while running, it will setup the flags which will be picked up by the system error check function*/
+void readSystemParameters()
+{
+  /*read the input voltages*/
+  inputVoltage = (float)analogRead(INPUT_VOLTAGE_FB_PIN) * INPUT_RESISTOR_DIVIDER * (float)voltageScalingFactor;
+
+  /*read the output voltages*/
+  outputVoltage = (float)analogRead(OUTPUT_VOLTAGE_FB_PIN) * OUTPUT_RESISTOR_DIVIDER * (float)voltageScalingFactor;
+
+  /*check for issues with the input voltages and set flags*/
+      if(inputVoltage >= inputVoltageHighValue)
+      {
+        runningError = true;
+        highInputVoltage = true;
+        lowInputVoltage = false;
+      }
+      else if(inputVoltage <= inputVoltageLowValue)
+      {
+        runningError = true;
+        lowInputVoltage = true; 
+        highInputVoltage = false;
+      }
+      else
+      {
+        lowInputVoltage = false;
+        highInputVoltage = false;
+      }
+      if(multiplier >= MULTIPLIER_MAX_VALUE)
+      {
+        if(outputVoltage <= OVERLOAD_VOLTAGE)
+        runningError = true;
+        overload = true;
+      }
+
+  /*check for issues with shorted outputs and set the flags*/    
+      if(digitalRead(SHORT_CIRCUIT_PIN) == LOW)
+      {
+        runningError = true;
+        shortCircuit = true;
+      }
+      else
+      {
+        shortCircuit = false;
+      }
+
+      if(shortCircuit == false && highInputVoltage == false && lowInputVoltage == false)
+      {
+        runningError = false;
+      }
+}
+
+void systemErrorCheck()
+{
+  /*check if there is any system error*/
+  if(startupError == true || runningError == true)
+  {
+    shutDown();
+  }
+}
+
+
+
+void shutDown()
+{
+  /* 
+   *  disable spwm
+      turn off mosfet driver and sg3525
+      shut down everything.
+      you must restart system to begin normal operation
+   *  
+   */
+
+  DDRB |= 1 << SHUT_DOWN_PIN;                               //shuts down external hardware
+  SREG &= ~(1 << 7);                                        //disable interrupts
+  TCCR1A = 0;                                               //disconnect the SPWM pins
+  TCCR1B = 0;
+  PORTB &= ~(1 << SPWM_OUTPUT_PIN_1);                       //pull both driver input low, incase the hardware shutdown fails 
+  PORTB &= ~(1 << SPWM_OUTPUT_PIN_2);
+}
+
+void PID(float& error)
+{
+
+    static int previousError = 0;
+    static uint8_t controller = 0;
+    static float integral = 0;
+    
+   /*prevent windup, don't integrate again if controller is almost saturated*/
+    if(controller >= SATURATION_MIN && controller <= SATURATION_MAX)                                                            
+      integral += error;
+
+   /*Ki = actual Ki * deltaTime and Kd = actual Kd / deltaTime*/
+    controller = constrain(Kp*error + Ki*integral + Kd*(error - previousError), 0, 500);        
+
+  /* write the controller output to the SPWM*/
+  /* you may wonder why i am writing the controller output to the PIDControllerOutput variable instead of the multiplier directly
+   *  it is because writing directly to the multiplier variable directly will update it immediately which may cause jitters in the sine wave
+   *  the PIDControllerOutput variable updates the SPWM every zero crossing point
+   *  you may also wonder why i compute the PID into the controller variable instead of the PIDControllerOutput variable directly
+   *  it is to avoid race conditions
+   */
+    PIDControllerOutput = controller;                                     
+    previousError = error;
+                                                         
+}
---
+++ INVERTER_V1.0/INVERTER_V1.0.ino
@@ -0,0 +1,97 @@
+/*This program is written with the intent of being used for the inverter, in no way do i assume it perfect by any margin.
+ * There is room for optimization, feel free to make copies and edit.
+ * use at your own risk haha :)
+ * 
+ * written by 
+ */
+
+#define SPWM_OUTPUT_PIN_1 PB1 
+#define SPWM_OUTPUT_PIN_2 PB2
+#define OUTPUT_VOLTAGE_FB_PIN A0                                                                //input voltage feedback pin
+#define INPUT_VOLTAGE_FB_PIN  A1                                                                //onput voltage feedback pin
+#define SATURATION_MAX 490                                                                      //constants used to avoid integration windup
+#define SATURATION_MIN 10
+#define SHORT_CIRCUIT_PIN PB4                                                                   //this pin is an input and is trigered by the hardware short circuit / overload circuit
+#define SHUT_DOWN_PIN PB5                                                                       //When the microsontroller sees a fault, it will disable the hardware by pulling this pin
+#define MULTIPLIER_MAX_VALUE 500                                                                //must be less than 500
+#define VOLTAGE_REFERENCE 5.0                                                                   //the arduino reference voltage for the ADC
+                                               
+
+/* output parameter settings */
+#define SET_OUTPUT_VOLTAGE  325                                                                  //325V peak not RMS since the voltage feedback is a divider and peak detection circuit [325 = 230*sqrt(2)]
+#define INPUT_RESISTOR_DIVIDER 6                                                                 //the resistor divider used in the DC input, i used a 10k and 2k giving a divsion on 6
+#define OUTPUT_RESISTOR_DIVIDER 201                                                              //resistor divider at the AC output, i used a 200k and 1k giving a division of approximately 201
+#define OVERLOAD_VOLTAGE 288                                                                     //if the microcontroller senses the voltage to be 288v peak (200v RMS) at the maximum duty cycle, there is overload
+
+
+/* PID parameters */
+#define K_proportional 4
+#define K_integral 1
+#define K_derivative 0
+#define deltaTime 0.04                                                                           //250hz sample time
+
+float voltageError = 0;
+const float Kp = K_proportional;
+const float Ki = K_integral * deltaTime;
+const float Kd = K_derivative / deltaTime;
+
+
+/*other variables*/
+volatile float PIDControllerOutput;
+volatile int multiplier = 0;
+volatile uint8_t sineArrayIndex = 0;
+volatile int sineHalf = 1;
+const float voltageScalingFactor =  float(VOLTAGE_REFERENCE / 1023.0);
+
+
+ /* function declarations*/
+void softStart(),
+     setupTimers(),
+     startupCheck(),
+     PID(float&),
+     systemErrorCheck(),
+     readSystemParameters();
+
+
+/*system parameters*/
+float inputVoltage,
+      inputVoltageLowValue = 12.0,              
+      inputVoltageHighValue = 14.0,
+      outputVoltage = 0;
+
+        
+/*system check errors*/                   //these can be used for troubleshooting
+bool startupError = false,                //can be caused by undevoltage or overvoltage
+     runningError = false,                //can be caused by undervoltage, overvoltage, shortcircuit or overloading
+     overload = false,
+     shortCircuit = false,
+     highInputVoltage = false,
+     lowInputVoltage = false,
+     overHeating = false;
+
+
+void setup()
+{ 
+  DDRB |= 1 << SPWM_OUTPUT_PIN_1;                                                        //Set pin 9 and 10 as output, OCR1A and OCR1B
+  DDRB |= 1 << SPWM_OUTPUT_PIN_2;
+  
+  DDRB &= ~(1  << SHORT_CIRCUIT_PIN);                                                    //set as input, will be toggled by hardware
+  PORTB |= 1 << SHORT_CIRCUIT_PIN;                                                       //set as PULLUP
+  
+  DDRB |= 1 << SHUT_DOWN_PIN;                                                            //this pin shuts down the IR2104 and SG3525, basically the whole system
+  PORTB &= ~(1 << SHUT_DOWN_PIN);
+  
+  startupCheck();                                                                        //checks that the input voltage is right
+  setupTimers();
+  softStart();                                                                           //slow start and checks for shortcircuit
+  
+}
+
+void loop() 
+{
+  readSystemParameters();
+  systemErrorCheck();
+  /*(SET_OUTPUT_VOLTAGE - outputVoltage) is the error, but since the output voltage can be set to different value, we can map the error between 0 and  its maximum value....this will help remove the need to retune the PID constants */
+  voltageError = map((SET_OUTPUT_VOLTAGE - outputVoltage), 0, SET_OUTPUT_VOLTAGE, 0, 100);                                       
+  PID(voltageError);  
+}
