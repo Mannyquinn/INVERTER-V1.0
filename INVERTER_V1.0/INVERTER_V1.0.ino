9dc0c516cbc10aac6e9458a905b9cb677f45ed3a
Mannyquinn <150354697+Mannyquinn@users.noreply.github.com>
2025-12-26 09:06:15 +0100

    Update INVERTER_V1.0.ino

--- INVERTER_V1.0/INVERTER_V1.0.ino
+++ INVERTER_V1.0/INVERTER_V1.0.ino
@@ -84,8 +84,11 @@ void setup()
   
   startupCheck();                                                                        //checks that the input voltage is right
   if(startupError == false)
+  {
+    
     setupTimers();
-  softStart();                                                                           //slow start and checks for shortcircuit
+    softStart();                                                                           //slow start and checks for shortcircuit
+  }
   
 }
 
@@ -98,4 +101,5 @@ void loop()
   voltageError = map((voltageDifference), 0, SET_OUTPUT_VOLTAGE, 0, 100);               //error is positive if voltageDifference is positive and negative if voltageDifference is                               
   PID(voltageError);  
 }
+
 
